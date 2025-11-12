ec4e51eeb29c481ee0be3443f82ded0c9ef050ac
Andrew-Chrom <chromovich1@gmail.com>
2025-11-12 19:25:11 +0200

    Fixed minor issue with article.body|slice:

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -9,9 +9,9 @@ def articles(req):
     return render(req, 'articles.html', {"articles": data})
 
 def view_article(req, id):
-    visible_len = 100
+    visible_len = 20
     
-    return render(req, 'view_article.html', {"article": Articles.objects.get(id=id), "visible_len": visible_len})
+    return render(req, 'view_article.html', {"article": Articles.objects.get(id=id)})
 
 def about(req):
     return render(req, 'about.html')
